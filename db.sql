60acf27732d4ae136a7f99dffad41ae053372ea8
a <139807472+noeln123@users.noreply.github.com>
2024-05-05 01:56:38 +0000

    Add files via upload

--- db.sql
+++ db.sql
@@ -17,12 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `blog`
---
-CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `blog`;
-
 -- --------------------------------------------------------
 
 --
